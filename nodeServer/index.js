50efe0a26d8117e8662a7949d17e6a7452f7bea9
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-02 14:42:24 +0530

    Update 1.7

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,10 +1,15 @@
+const { use } = require('react');
 const { Socket } = require('socket.io');
 
 // Node server Which will handle socket io connections 
 const io = require('socket.io')(8000)
 const users ={};
 io.on('connection', Socket =>{
-    Socket.on('user-joined',name =>{
-        
+    Socket.on('new-user-joined',name =>{
+        users[Socket.id] = name;
+        Socket.broadcast.emit('user-joined',name);
+    })
+    Socket.on('send',message =>{
+        Socket.broadcast.emit('receive',{message: message,name: user[Socket.id]})
     })
 })
